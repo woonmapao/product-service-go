be85a90a40a8000711547db4dc79bf089c69bdc7
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-25 12:25:25 +0700

    Switch code that were paste wrong

--- initializer/databaseInitializer.go
+++ initializer/databaseInitializer.go
@@ -2,13 +2,21 @@ package initializer
 
 import (
 	"log"
+	"os"
 
-	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func LoadEnvVariables() {
-	err := godotenv.Load()
+var DB *gorm.DB
+
+func DBInitializer() {
+
+	dsn := os.Getenv("DB_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Error loading .env files")
+		log.Fatal(err)
 	}
+
+	DB = db
 }
