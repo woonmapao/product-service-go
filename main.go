e4cd3714e76832135f7f8b3164b0eaf2165e8004
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-25 15:36:33 +0700

    Added cool ASCII

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/woonmapao/product-service-go/initializer"
 )
+
+func init() {
+	initializer.LoadEnvVariables()
+	initializer.DBInitializer()
+}
 
 func main() {
 	r := gin.Default()
